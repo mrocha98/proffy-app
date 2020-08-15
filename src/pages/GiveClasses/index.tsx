1990837440be03d8ebd61f354f2fc96c304d8711
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-15 01:32:00 -0300

    finish fourth video

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+import styles from "./styles";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
+  const handleNavigateBack = () => goBack();
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBgImage}
+        resizeMode="contain"
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um profy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor em nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+};
+
+export default GiveClasses;
